08a0d0398d1f7b407c500341a64e621cf05c7d72
予纾 <yushu@srcz.one>
2026-04-14 10:18:41 +0800

    fix: Update properties in ChartState to remove required modifier and allow null values

--- src/Prober/DivingFish/Models/ChartState.cs
+++ src/Prober/DivingFish/Models/ChartState.cs
@@ -5,26 +5,26 @@ namespace Limekuma.Prober.DivingFish.Models;
 public record ChartState
 {
     [JsonPropertyName("cnt")]
-    public required double SampleSize { get; init; }
+    public double SampleSize { get; init; }
 
     [JsonPropertyName("diff")]
-    public required string Level { get; init; }
+    public string? Level { get; init; }
 
     [JsonPropertyName("fit_diff")]
-    public required double FitLevel { get; init; }
+    public double FitLevel { get; init; }
 
     [JsonPropertyName("avg")]
-    public required double AverageAchievements { get; init; }
+    public double AverageAchievements { get; init; }
 
     [JsonPropertyName("avg_dx")]
-    public required double AverageDXScore { get; init; }
+    public double AverageDXScore { get; init; }
 
     [JsonPropertyName("std_dev")]
-    public required double StandardDeviation { get; init; }
+    public double StandardDeviation { get; init; }
 
     [JsonPropertyName("dist")]
-    public required List<int> RankDistribution { get; init; }
+    public List<int>? RankDistribution { get; init; }
 
     [JsonPropertyName("fc_dist")]
-    public required List<double> ComboDistribution { get; init; }
+    public List<double>? ComboDistribution { get; init; }
 }
